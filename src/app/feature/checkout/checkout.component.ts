88ec70f36fb75f82ed53a92bb2b083497e80a4e4
RexVargas <rvargas.itq@gmail.com>
2024-03-10 16:26:25 -0600

    feat: integrate the checkout to remove items from cart

--- src/app/feature/checkout/checkout.component.ts
+++ src/app/feature/checkout/checkout.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { CartStore } from '@shared/store/shopping-cart.store';
+import { CheckoutService } from './services/checkout.service';
 
 @Component({
   selector: 'app-checkout',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.scss'
 })
 export class CheckoutComponent {
+  cartStore = inject(CartStore);
+  private readonly _checkoutService = inject(CheckoutService);
 
+
+  clearAll = () => {
+    this.cartStore.clearCart();
+  }
+
+  removeItem = (id: number) => {
+    this.cartStore.removeFromCart(id);
+  }
+
+  onProceedToPay = () => {
+    this._checkoutService.onProceedToPay();
+  }
 }
