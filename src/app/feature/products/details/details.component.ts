eba4611adf575ea2990b586ff64c21949a9d4893
RexVargas <rvargas.itq@gmail.com>
2024-03-10 15:20:04 -0600

    feat - generate the start according to reates

--- src/app/feature/products/details/details.component.ts
+++ src/app/feature/products/details/details.component.ts
@@ -1,5 +1,6 @@
 import { CurrencyPipe } from '@angular/common';
 import { Component, OnInit, Signal, inject, input } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { ProductsService } from '@api/services/products.service';
 import { Product } from '@shared/interfaces/product';
 
@@ -19,12 +20,43 @@ export class DetailsComponent implements OnInit{
   product!: Signal<Product | undefined>;
 
   private readonly productService = inject(ProductsService);
+  private readonly _sanitizer = inject(DomSanitizer);
 
   ngOnInit(): void {
     this.product = this.productService.getProductById(this.productId());
   }
 
-  generateSVG = (index: number) => {}
+  generateSvg = (index: number): SafeHtml => {
+    console.log('run')
+    let SVGContent = null;
+    const rate = this.product()?.rating.rate as number;
+
+    if (index + 1 <= Math.floor(rate)) {
+      SVGContent = `<svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+      stroke-width="2" class="w-4 h-4 text-yellow-400" viewBox="0 0 24 24">
+      <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z">
+      </path>
+    </svg>`;
+    } else if ( index < rate) {
+      SVGContent = `<svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-yellow-400" viewBox="0 0 24 24">
+      <defs>
+        <linearGradient id="partialFillGradient" x1="0%" y1="0%" x2="100%" y2="0%">
+          <stop offset="50%" style="stop-color:currentColor; stop-opacity:1" />
+          <stop offset="50%" style="stop-color:currentColor; stop-opacity:0" />
+        </linearGradient>
+      </defs>
+      <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z" fill="url(#partialFillGradient)"></path>
+    </svg>`;
+    } else {
+      SVGContent = `<svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+              class="w-4 h-4 text-yellow-400" viewBox="0 0 24 24">
+              <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z">
+              </path>
+            </svg>`
+    }
+
+    return this._sanitizer.bypassSecurityTrustHtml(SVGContent);
+  }
 
   onAddToCart = () => {}
 }
